744dce1f9ccea24d39cd19b349b69ad4176b10b4
Manuel Martinez <salutte@gmail.com>
2023-02-21 07:48:28 +0100

    migrated to a more robust drift detection, which probably should simply be eliminated. Also added support for the right timescales.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,32 @@ use csv;
 use std::{error::Error, fs::File, io::BufWriter, path::PathBuf, io};
 use vcd::{ self, TimescaleUnit, SimulationCommand };
 use regex::Regex;
-use num_traits::PrimInt;
 
-struct RigolDataSeries {
-    timestamp: f64,
-    signals: u16,
+struct RigolTSData {
+	timestamp: f64,
+	data: u16
 }
 
-fn read_rigol_csv<R: io::Read>(io_reader: R) -> Result<Vec<RigolDataSeries>, Box<dyn Error>> {
+struct RigolCSVData {
+	
+	t0 : f64,
+    t_inc : f64,
+	signals: Vec<RigolTSData>
+}
+
+fn read_rigol_csv<R: io::Read>(io_reader: R) -> Result<RigolCSVData, Box<dyn Error>> {
 	
     let mut rdr = csv::ReaderBuilder::new().flexible(true).from_reader(io_reader);
 
     // TODO: Handle CSV when timestamps are in each row (enum/option)
     // Initial timestamp...
     
-    let t0;
-    let t_inc;
-
+    let mut rigol = RigolCSVData {
+		t0 : 0.0,
+		t_inc : 0.0,
+		signals : vec![]
+	};
+    
 	let mut i_csv = None;
 
 	let mut i_d7_d0 = None;
@@ -49,25 +58,27 @@ fn read_rigol_csv<R: io::Read>(io_reader: R) -> Result<Vec<RigolDataSeries>, Box
 				_ => Err("Unknown header {header}"),
 			}?
 		}
-		t0 = t0_option.unwrap();
-		t_inc = t_inc_option.unwrap();
+		rigol.t0 = t0_option.unwrap();
+		rigol.t_inc = t_inc_option.unwrap();
 	}
 	
     
-    println!("Initial timestamp {t0} with increments of {t_inc} seconds");
+    println!("Initial timestamp {} with increments of {} seconds", rigol.t0, rigol.t_inc);
 
-    let mut signals: Vec<RigolDataSeries> = vec![];
-
-	let mut t_now = t0;
-    for row in rdr.records() {
+	let mut async_count = 0;
+	
+    for (i, row) in rdr.records().enumerate() {
 		
 		let r = row?;
 
-		t_now = t_now + t_inc;
+		let t_now = rigol.t0 + ((i) as f64) * rigol.t_inc;
         let t_csv = r[i_csv.unwrap()].parse::<f64>()?;
         
-        assert!( (t_now - t_csv).abs() < t_inc);
-                
+        //println!("{} == {}", format!("{:+0.2e}", t_now), format!("{:+0.2e}", t_csv));
+        async_count = async_count + 1;
+		if format!("{:+0.4e}", t_now) == format!("{:+0.4e}", t_csv) { async_count = 0; }
+		assert!(async_count < 5);
+        
         let mut d_all : u16 = 0;
         
         if i_d7_d0.is_some() { d_all += r[i_d7_d0.unwrap()].parse::<f64>()? as u16; }
@@ -76,15 +87,15 @@ fn read_rigol_csv<R: io::Read>(io_reader: R) -> Result<Vec<RigolDataSeries>, Box
         if i_d15_d8.is_some() { d_all += ( r[i_d15_d8.unwrap()].parse::<f64>()? as u16) << 8; }
         if i_d8_d15.is_some() { d_all += ( r[i_d8_d15.unwrap()].parse::<f64>()? as u16) << 8; }
         
-        signals.push(RigolDataSeries { timestamp: t_csv, signals: d_all } );
+        rigol.signals.push(RigolTSData { timestamp: t_now, data: d_all } );
     }
 	
 	println!("Signals read");
 
-    Ok(signals)
+    Ok(rigol)
 }
 
-fn write_vcd<W: io::Write>(io_writer: &mut W, sigs: Vec<RigolDataSeries>) -> Result<(), Box<dyn Error>> {
+fn write_vcd<W: io::Write>(io_writer: &mut W, rigol: RigolCSVData) -> Result<(), Box<dyn Error>> {
 
 	fn vcd_vector_from_u16(d : u16) -> Vec<vcd::Value> {
 		
@@ -97,7 +108,7 @@ fn write_vcd<W: io::Write>(io_writer: &mut W, sigs: Vec<RigolDataSeries>) -> Res
 
 	
     // Write the header
-    writer.timescale(1, TimescaleUnit::US)?;
+    writer.timescale( (rigol.t_inc / 1e-12) as u32, TimescaleUnit::PS)?;
     writer.add_module("top")?;
     let data = writer.add_wire(16, "data")?;
     writer.upscope()?;
@@ -105,21 +116,21 @@ fn write_vcd<W: io::Write>(io_writer: &mut W, sigs: Vec<RigolDataSeries>) -> Res
   
     // Write the initial values
     writer.begin(SimulationCommand::Dumpvars)?;
-    writer.change_vector(data, &vcd_vector_from_u16(sigs[0].signals))?;
+    writer.change_vector(data, &vcd_vector_from_u16(rigol.signals[0].data))?;
     writer.end()?;
   
-    let offset = sigs[0].timestamp;
+    let offset = rigol.signals[0].timestamp;
     // Write the data values
-    for s in sigs {
+    for s in rigol.signals {
       // TODO: Tweak that 10000000 with the defined timescale in the header
       writer.timestamp(((s.timestamp-offset).abs() * 1000000000.0) as u64)?;
-      writer.change_vector(data,  &vcd_vector_from_u16(s.signals))?;
+      writer.change_vector(data,  &vcd_vector_from_u16(s.data))?;
     }
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let sigs = read_rigol_csv(io::stdin())?;
-    write_vcd(&mut BufWriter::new(File::create(PathBuf::from("data/test.vcd"))?), sigs)?;
+    let rigol_data = read_rigol_csv(io::stdin())?;
+    write_vcd(&mut BufWriter::new(File::create(PathBuf::from("data/test.vcd"))?), rigol_data)?;
     Ok(())
 }
